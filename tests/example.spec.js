d2eb23f78d41c69a67c7b3f8cb0f498b1f77cc60
Booruno <1brunoocunha@gmail.com>
2025-12-16 16:46:32 -0300

    cenario de testes criado via record

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,16 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Realizar login com sucesso', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('testes@gmail.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('checkbox', { name: 'Lembrar de mim' }).check();
+  await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('heading', { name: 'Login realizado' }).click();
+  await page.getByText('Olá, testes@gmail.com').click();
+  await page.getByRole('button', { name: 'OK' }).click();
 });
